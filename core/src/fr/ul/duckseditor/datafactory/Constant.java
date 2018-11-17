dafafd12c3ccc6c2a2c2081a3b165db7a20f1668
mohamed sarr <mohamedsarr0012@gmail.com>
2018-11-17 01:01:21 +0000

    //

---
+++ core/src/fr/ul/duckseditor/datafactory/Constant.java
@@ -0,0 +1,14 @@
+package fr.ul.duckseditor.datafactory;
+
+public class Constant {
+    public static final float PIXEL_TO_METER=100;
+    public static  final int WORLD_WIDTH=800;
+    public static  final int WORLD_HEIGTH=480;
+    public static float DUCK_WIDTH=WORLD_WIDTH/15;
+    public static float DUCK_HEIGHT=WORLD_HEIGTH/4;
+    public static  final float CARRE_WIDTH=2;
+    public static  final float RECTANGLE_WIDTH=1;
+    public static  final float RECTANGLE_HEIGHT=4;
+    //public static int DUCK_POS_X=
+    //public static int DUCK_POS=;
+}
